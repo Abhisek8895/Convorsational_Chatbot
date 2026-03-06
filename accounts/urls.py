b3d8e72219d630f51db980a8d1f8939a5df34e76
Abhisek8895 <mishraabhisek14@gmail.com>
2026-03-06 21:57:19 +0530

    Implemented Logout functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import signup_view, login_view, home, landing_view
+from .views import signup_view, login_view, home, landing_view,logout_view
 
 urlpatterns = [
     path("", landing_view, name="landing"),
     path("signup/", signup_view, name="signup"),
     path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
     path("home/", home, name="home"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login,logout
 from django.contrib.auth.decorators import login_required
 
 def signup_view(request):
@@ -41,3 +41,7 @@ def home(request):
 
 def landing_view(request):
     return render(request, "landing.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect("landing")
