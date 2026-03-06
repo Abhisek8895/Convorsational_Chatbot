63a525d3f4abd317ab3158cbe92833f6976f2126
Abhisek8895 <mishraabhisek14@gmail.com>
2026-03-06 21:19:44 +0530

    Implemented Login system

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
-
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
 def signup_view(request):
     if request.method == "POST":
@@ -14,3 +16,25 @@ def signup_view(request):
         form = SignupForm()
 
     return render(request, "signup.html", {"form": form})
+
+
+
+def login_view(request):
+
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "login.html", {"form": form})
+
+
+@login_required
+def home(request):
+    return render(request, "home.html")
